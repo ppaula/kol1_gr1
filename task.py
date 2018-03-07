a1c9d63f3f9b88ba01a8ac75885408b136014e22
ppaula <palcowskapaula@gmail.com>
2018-03-07 18:36:30 +0100

    Update task.py
    task

--- task.py
+++ task.py
@@ -24,3 +24,14 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+from matrix import Matrix
+
+matrix_1 = Matrix(4,5,6,7)
+matrix_2 = Matrix(2,2,2,1)
+matrix_1.print()
+matrix_2.print()
+matrix_3 = matrix_2.add(matrix_1)
+matrix_3.print()
+matrix_4 = matrix_2.prod(matrix_1)
+matrix_4.print()
