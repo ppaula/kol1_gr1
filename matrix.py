8c67790bba4c16b521e65f9b063891ea7acd9f71
ppaula <palcowskapaula@gmail.com>
2018-03-22 20:41:28 +0100

    Update matrix.py
    poprawa

--- matrix.py
+++ matrix.py
@@ -1,24 +1,74 @@
+import numpy as np
+
 class Matrix:
 
-	a = 0
-	b = 0
-	c = 0
-	d = 0
+	dimention = 0
+	matrix = list()
 
-	def __init__(self, a, b, c, d):
-		self.a = a
-		self.b = b
-		self.c = c
-		self.d = d
+	def __init__(self, *matrix_elements):
+		self.dimention = int(len(matrix_elements) ** (1/2))
+		self.matrix = [[0 for x in range(self.dimention)] for y in range(self.dimention)] 
+		for x in range(0, self.dimention):
+			for y in range(0, self.dimention):
+				self.matrix[x][y] = matrix_elements[x*self.dimention + y]
 
-	def print(self):
-		print("[", self.a, ", ", self.b, "][", self.c, ", ", self.d, "]")
+	def __str__(self):
+		return str(self.matrix)
 
-	def add(self, matrix):
-		matrix_2 = Matrix(self.a + matrix.a, self.b + matrix.b, self.c + matrix.c, self.d + matrix.d)
-		return matrix_2
+	def add(self, matrix_to_add):
+		if self.dimention == matrix_to_add.dimention:
+			matrix_to_return = list()
+			for x in range(0, self.dimention):
+				for y in range(0, self.dimention):
+					matrix_to_return.append(self.matrix[x][y] + matrix_to_add.matrix[x][y])
+			return Matrix(*matrix_to_return)
+		else:
+			raise Exception('Inconsistent agrument!')
 
-	def prod(self, matrix):
-		matrix_2 = Matrix(self.a * matrix.a + self.b * matrix.c, self.b * matrix.b + self.b * matrix.d, self.c * matrix.c + self.d * matrix.c, self.d * matrix.d + self.c * matrix.b)
-		return matrix_2
+	def subtract(self, matrix_to_subtract):
+		if self.dimention == matrix_to_subtract.dimention:
+			matrix_to_return = list()
+			for x in range(0, self.dimention):
+				for y in range(0, self.dimention):
+					matrix_to_return.append(self.matrix[x][y] - matrix_to_subtract.matrix[x][y])
+			return Matrix(*matrix_to_return)
+		else:
+			raise Exception('Inconsistent agrument!')
+
+	def dummy_multiply(self, matrix_to_multiply):
+		if self.dimention == matrix_to_multiply.dimention:
+			matrix_to_return = list()
+			for x in range(0, self.dimention):
+				for y in range(0, self.dimention):
+					matrix_to_return.append(self.matrix[x][y] * matrix_to_multiply.matrix[x][y])
+			return Matrix(*matrix_to_return)
+		else:
+			raise Exception('Inconsistent agrument!')
+
+	def multiply(self, matrix_to_multiply):
+		if self.dimention == matrix_to_multiply.dimention:
+			matrix_to_return = np.zeros(self.dimention ** 2,)
+			for x in range(0, self.dimention):
+				for y in range(0, self.dimention):
+					for z in range(0, self.dimention):
+							matrix_to_return[x*self.dimention + y] += self.matrix[x][z] * matrix_to_multiply.matrix[z][y]
+			return Matrix(*matrix_to_return)
+		else:
+			raise Exception('Inconsistent agrument!')
+	
+	def add_one(self):
+		for x in range(0, self.dimention):
+			for y in range(0, self.dimention):
+				self.matrix[x][y] += 1
+	
+	def subtract_one(self):
+		for x in range(0, self.dimention):
+			for y in range(0, self.dimention):
+				self.matrix[x][y] = 1 - self.matrix[x][y]
+	
+	def double_matrix(self):
+		for x in range(0, self.dimention):
+			for y in range(0, self.dimention):
+				self.matrix[x][y] += self.matrix[x][y]
+
 
